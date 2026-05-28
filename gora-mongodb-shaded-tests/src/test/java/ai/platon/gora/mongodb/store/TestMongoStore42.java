91bb5106a768cda5d1bc44d7f25be486ee1e5e00
platon.AI <galaxyeye@live.cn>
2026-05-28 14:01:56 +0800

    Add gora-mongodb-shaded-tests

---
+++ gora-mongodb-shaded-tests/src/test/java/ai/platon/gora/mongodb/store/TestMongoStore42.java
@@ -0,0 +1,48 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ai.platon.gora.mongodb.store;
+
+import ai.platon.gora.mongodb.GoraMongodbTestDriver;
+import ai.platon.gora.mongodb.utils.BSONDecorator;
+import org.bson.Document;
+import org.junit.Test;
+import org.testcontainers.mongodb.MongoDBContainer;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Perform {@link TestMongoStore} tests on MongoDB 4.2.x server.
+ */
+public class TestMongoStore42 extends TestMongoStore {
+
+  private static final MongoDBContainer container = new MongoDBContainer("mongo:4.2");
+
+  static {
+    setTestDriver(new GoraMongodbTestDriver(container));
+  }
+
+  @Test
+  public void testFromMongoList_null() throws Exception {
+    MongoStore store = new MongoStore();
+    Document noField = new Document();
+    String field = "myField";
+    Object item = store.fromMongoList(field, null, new BSONDecorator(noField),
+            null);
+    assertNotNull(item);
+  }
+}
---
+++ gora-mongodb-shaded-tests/src/test/java/ai/platon/gora/mongodb/store/TestMongoStoreMappingFromProperties.java
@@ -0,0 +1,89 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ai.platon.gora.mongodb.store;
+
+import junit.framework.Assert;
+import org.apache.commons.io.IOUtils;
+import ai.platon.gora.examples.generated.Employee;
+import ai.platon.gora.store.DataStoreFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.testcontainers.mongodb.MongoDBContainer;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Properties;
+
+/**
+ * Test case for loading mappings from properties
+ */
+public class TestMongoStoreMappingFromProperties {
+
+    public final static MongoDBContainer _container = new MongoDBContainer("mongo:4.2");
+
+    @BeforeClass
+    public static void setUpClass() {
+        _container.start();
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        if (_container.isRunning()) {
+            _container.stop();
+        }
+    }
+
+    @Test
+    public void testInitialize() throws IOException {
+        // Simple mapping XML
+        String mappingXml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<gora-otd>\n" +
+                "    <class name=\"ai.platon.gora.examples.generated.Employee\" keyClass=\"java.lang.String\" document=\"frontier\">\n" +
+                "        <field name=\"name\" docfield=\"name\" type=\"string\"/>\n" +
+                "    </class>\n" +
+                "</gora-otd>";
+
+        // Initiate the MongoDB server on the default port
+        int port = _container.getFirstMappedPort();
+        String host = _container.getHost();
+
+        Properties prop = DataStoreFactory.createProps();
+
+        // Store Mongo server "host:port" in Hadoop configuration
+        // so that MongoStore will be able to get it latter
+        Configuration conf = new Configuration();
+        conf.set(MongoStoreParameters.PROP_MONGO_SERVERS, host + ":" + port);
+
+        // Set mapping XML property
+        prop.setProperty(MongoStore.XML_MAPPING_DEFINITION, mappingXml);
+        MongoStore<String, Employee> mongoStore =
+                DataStoreFactory.createDataStore(MongoStore.class, String.class, Employee.class, conf, prop);
+        MongoMapping actualMapping = mongoStore.getMapping();
+
+        // Read mapping definition from mappingXml
+        MongoMappingBuilder<String, Employee> builder = new MongoMappingBuilder<>(mongoStore);
+        builder.fromInputStream(IOUtils.toInputStream(mappingXml, (Charset) null));
+        MongoMapping expectedMapping = builder.build();
+
+        Assert.assertEquals(expectedMapping, actualMapping);
+    }
+
+}
